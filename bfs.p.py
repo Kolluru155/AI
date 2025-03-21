a78bf1fefaa7d4370737ed98e20724b292bf1402
Kolluru155 <knavya155@gmail.com>
2025-03-21 09:09:31 +0530

    Add files via upload

---
+++ bfs.p.py
@@ -0,0 +1,22 @@
+from collections import deque
+
+def bfs(graph, start):
+    visited, queue = set(), deque([start])
+    while queue:
+        node = queue.popleft()
+        if node not in visited:
+            print(node, end=" ")
+            visited.add(node)
+            queue.extend(neighbor for neighbor in graph[node] if neighbor not in visited)
+
+graph = {
+    'A': ['B', 'C'],
+    'B': ['A', 'D', 'E'],
+    'C': ['A', 'F'],
+    'D': ['B'],
+    'E': ['B', 'F'],
+    'F': ['C', 'E']
+}
+
+print("BFS Traversal:")
+bfs(graph, 'A')
